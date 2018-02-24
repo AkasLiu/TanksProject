8238290c882d824b30ac4b90efc43506530cab75
ARCUS <929491690@qq.com>
2018-02-24 18:27:28 +0800

    shell add

---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMovement : MonoBehaviour {
+
+    public float speed = 5;
+    public float angularSpeed = 30;
+    private Rigidbody rigidbody;
+    public float number = 1;    //
+     
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = this.GetComponent<Rigidbody>();
+	}
+
+    void FixedUpdate()
+    {
+        float v = Input.GetAxis("VerticalPlayer"+number);
+        rigidbody.velocity = transform.forward * v * speed; //transform.forward 代表前方向  h代表-1到1的值
+
+        float h = Input.GetAxis("HorizontalPlayer"+number);
+        rigidbody.angularVelocity = transform.up * h * angularSpeed;
+    }
+}
---
+++ Assets/TankAttack.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankAttack : MonoBehaviour {
+
+    public GameObject shellPrefab;
+    private Transform firePostion;
+    public KeyCode fireKey=KeyCode.Space;
+
+	// Use this for initialization
+	void Start () {
+        firePostion = transform.Find("FirePosition");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(fireKey))
+        {
+            GameObject.Instantiate(shellPrefab,firePostion.position,firePostion.rotation);
+        }
+	}
+}
